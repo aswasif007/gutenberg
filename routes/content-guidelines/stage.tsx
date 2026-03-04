97bb20de23ba0f06a8e26a7c7f058bf6e1aed4d3
Mayank Chugh <mayank.chugh@automattic.com>
2026-03-04 17:51:37 +0000

    Content Guidelines: Add actions section UI

--- routes/content-guidelines/stage.tsx
+++ routes/content-guidelines/stage.tsx
@@ -14,6 +14,7 @@ import './style.scss';
 import GuidelineAccordion from './components/guideline-accordion';
 import GuidelineAccordionForm from './components/guideline-accordion-form';
 import { fetchContentGuidelines } from './api';
+import ActionsSection from './components/actions-section';
 
 const GUIDELINE_ITEMS = [
 	{
@@ -131,6 +132,7 @@ function ContentGuidelinesPage() {
 							} ) }
 						</ul>
 						{ /* eslint-enable jsx-a11y/no-redundant-roles */ }
+						<ActionsSection />
 					</div>
 				)
 			) }
